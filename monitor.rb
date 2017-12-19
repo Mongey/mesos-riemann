0d23f018adde1a0c976ec061dc1d539d3a504129
Conor Mongey <conor@mongey.net>
2017-12-19 16:17:49 +0000

    Make code less awful

--- monitor.rb
+++ monitor.rb
@@ -7,6 +7,79 @@ RIEMANN_ADDRESS = ENV.fetch('RIEMANN_ADDRESS')
 RIEMANN_PORT = ENV.fetch('RIEMANN_PORT', '5555').to_i
 c = Riemann::Client.new host: RIEMANN_ADDRESS, port: RIEMANN_PORT, timeout: 5
 
+def task_name_from_container(env_vars)
+  marathon_env = env_vars.select { |env| env.start_with? 'MARATHON_APP_ID' }
+  return unless marathon_env.any?
+
+  marathon_id = marathon_env.first.split('=').last
+  marathon_names = marathon_id.split('/').reject { |e| e == '' }.compact
+
+  marathon_group = marathon_names.first if marathon_names.size > 1
+  marathon_task = marathon_names.last
+  marathon_group.nil? ? marathon_task : "#{marathon_group}-#{marathon_task}"
+end
+
+def stats_for_container(container_id)
+  stats = Docker::Util.parse_json(container.connection.get("/containers/#{container_id}/stats", stream: false))
+  memory_stats = stats['memory_stats']
+  memory_usage = memory_stats['usage'].to_f
+  memory_total = memory_stats['limit'].to_f
+  memory_percentage = memory_usage / memory_total
+
+  state = container_json['State']
+  running = state['Running']
+
+  if running
+    start_time = DateTime.rfc3339(state['StartedAt']).to_time.utc.to_i
+    now = DateTime.now.to_time.utc.to_i
+    uptime = now - start_time
+  end
+
+  {
+    running: running,
+    uptime: uptime,
+    memory_usage: memory_usage,
+    memory_total: memory_total,
+    memory_percentage: memory_percentage,
+    net_rx: stats['network']['rx_bytes'],
+    net_tx: stats['network']['tx_bytes'],
+  }
+end
+
+def stats_to_events(stats, name, host)
+  prefix = "docker #{name}"
+  events = [
+    {
+      service: "#{prefix} mem usage",
+      metric: stats[:memory_usage]
+    },
+    {
+      service: "#{prefix} mem total",
+      metric: stats[:memory_total]
+    },
+    {
+      service: "#{prefix} mem ratio",
+      metric: stats[:memory_percentage]
+    },
+    {
+      service: "#{prefix} net rx",
+      metric: stats[:net_rx]
+    },
+    {
+      service: "#{prefix} net tx",
+      metric: stats[:net_tx]
+    }
+  ]
+
+  if stats[:running]
+    events << {
+      service: "#{prefix} uptime",
+      metric: stats[:uptime]
+    }
+  end
+  events.map {|e| e[:host] = host; e}
+end
+
 def log(container, c)
   begin
     container_json = container.json
@@ -17,67 +90,12 @@ def log(container, c)
   return unless container_json
 
   env_vars = container_json['Config']['Env']
-  marathon_env = env_vars.select { |env| env.start_with? 'MARATHON_APP_ID' }
-  return unless marathon_env.any?
-
-  marathon_id = marathon_env.first.split('=').last
-  marathon_names = marathon_id.split('/').reject { |e| e == '' }.compact
-
-  marathon_group = marathon_names.first if marathon_names.size > 1
-  marathon_task = marathon_names.last
+  riemann_marathon_name = task_name_from_container(env_vars)
+  return unless riemann_marathon_name
 
   host = env_vars.select { |e| e.start_with? 'HOST' }.first.split('=').last
-  riemann_marathon_name = marathon_group.nil? ? marathon_task : "#{marathon_group}-#{marathon_task}"
-
-  stats = Docker::Util.parse_json(container.connection.get("/containers/#{container.id}/stats", stream: false))
-
-  memory_stats = stats['memory_stats']
-  memory_usage = memory_stats['usage'].to_f
-  memory_total = memory_stats['limit'].to_f
-  memory_percentage = memory_usage / memory_total
-
-  state = container_json['State']
-  running = state['Running']
-  if running
-    start_time = DateTime.rfc3339(state['StartedAt']).to_time.utc.to_i
-    now = DateTime.now.to_time.utc.to_i
-    uptime = now - start_time
-
-    c.tcp << {
-      host: host,
-      service: "docker #{riemann_marathon_name} uptime",
-      metric: uptime
-    }
-  end
-
-  c.tcp << {
-    host: host,
-    service: "docker #{riemann_marathon_name} mem usage",
-    metric: memory_usage
-  }
-
-  c.tcp << {
-    host: host,
-    service: "docker #{riemann_marathon_name} mem total",
-    metric: memory_total
-  }
-
-  c.tcp << {
-    host: host,
-    service: "docker #{riemann_marathon_name} net rx",
-    metric: stats['network']['rx_bytes']
-  }
-  c.tcp << {
-    host: host,
-    service: "docker #{riemann_marathon_name} net tx",
-    metric: stats['network']['tx_bytes']
-  }
-
-  c.tcp << {
-    host: host,
-    service: "docker #{riemann_marathon_name} mem ratio",
-    metric: memory_percentage
-  }
+  stats = stats_for_container(container.id)
+  c.tcp << stats_to_events(stats, riemann_marathon_name, host)
 end
 
 loop do
